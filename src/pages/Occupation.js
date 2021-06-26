a7955f6b0ea5ff13742c1a54e766fdb19098a562
angzhang <angzhang@adobe.com>
2021-06-26 14:39:15 -0700

    prelim occupation page done

--- src/pages/Occupation.js
+++ src/pages/Occupation.js
@@ -8,64 +8,72 @@ function Occupation (props) {
             <Header/>
             </header>           
             <div className="content-container-white"> 
-                <div>
-                    <h1>Agriculture and Food Scientists</h1>
+                <div className="occupation-page-container">
+                    <h1 className="blue-underline-text">Agriculture and Food Scientists</h1>
                     <p>BLS group</p> 
-                    <p>Agricultural and food scientists create scientific methods and research strategies to establish more efficient agricultural services and products.</p>
-                    <div className="statistics-container">
-                        <h2>Statistics</h2> 
-                        <p><b>Annual Income Earned (median)</b></p>
-                        <p><b>Total Employment</b></p>
-                        <p><b>Education (mean)</b></p>
-                        <p><b>Minimum Degree Attainment</b></p>
-                        <p><b>Welfare (mean)</b></p>
-                        <p><b>Overall Health Insurance Coverage (percentage)</b></p>
-                        <p><b>Public Health Insurance Coverage (percentage)</b></p>
-                        <p><b>US Citizenship (percentage)</b></p>
+                    <div className="occ-sub-container"> 
+                        <p>Agricultural and food scientists create scientific methods and research strategies to establish more efficient agricultural services and products.</p>
                     </div>
-                    <h2>Worker Characteristics</h2>
-                    <table className="characteristics-table">
-                        <tr className="characteristic-row">
-                            <th className="characteristic-table-name">Characteristic</th>
-                            <th className="characteristic-table-score">O-NET Score</th>
-                        </tr>
-                        <tr className="characteristic-row">
-                            <td className="characteristic-table-name">Characteristic</td>
-                            <td className="characteristic-table-score">O-NET Score</td>
-                        </tr>
-                        <tr className="characteristic-row">
-                            <td className="characteristic-table-name">Characteristic</td>
-                            <td className="characteristic-table-score">O-NET Score</td>
-                        </tr>
-                        <tr className="characteristic-row">
-                            <td className="characteristic-table-name">Characteristic</td>
-                            <td className="characteristic-table-score">O-NET Score</td>
-                        </tr>
-                        <tr className="characteristic-row">
-                            <td className="characteristic-table-name">Characteristic</td>
-                            <td className="characteristic-table-score">O-NET Score</td>
-                        </tr>
-                        <tr className="characteristic-row">
-                            <td className="characteristic-table-name">Characteristic</td>
-                            <td className="characteristic-table-score">O-NET Score</td>
-                        </tr>
-                    </table>
-                    <h2>Worker Characteristic Graphs</h2>
-                    {/* exposure to climate for food occupation */}
-                    <div className="occupation-graph">
-                        <img alt="exposure to climate of food system worker" src="/test/exposure_to_climate.png"></img>
+                    <div className="occ-sub-container"> 
+                        <h2 className="blue-text">Statistics</h2>
+                        <div className="stats-container"> 
+                            <p><b>Annual Income Earned (median)</b></p>
+                            <p><b>Total Employment</b></p>
+                            <p><b>Education (mean)</b></p>
+                            <p><b>Minimum Degree Attainment</b></p>
+                            <p><b>Welfare (mean)</b></p>
+                            <p><b>Overall Health Insurance Coverage (percentage)</b></p>
+                            <p><b>Public Health Insurance Coverage (percentage)</b></p>
+                            <p><b>US Citizenship (percentage)</b></p>
+                        </div>
                     </div>
-                    {/* gender */}
-                    <div className="occupation-graph">
-                        <img alt="exposure to climate of food system worker" src="/test/exposure_to_climate.png"></img>
+                    <div className="occ-sub-container"> 
+                    <h2 className="blue-text">Worker Characteristics</h2>
+                        <table className="characteristics-table">
+                            <tr className="characteristic-row">
+                                <th className="characteristic-table-name">Characteristic</th>
+                                <th className="characteristic-table-score">O-NET Score</th>
+                            </tr>
+                            <tr className="characteristic-row">
+                                <td className="characteristic-table-name">Characteristic</td>
+                                <td className="characteristic-table-score">O-NET Score</td>
+                            </tr>
+                            <tr className="characteristic-row">
+                                <td className="characteristic-table-name">Characteristic</td>
+                                <td className="characteristic-table-score">O-NET Score</td>
+                            </tr>
+                            <tr className="characteristic-row">
+                                <td className="characteristic-table-name">Characteristic</td>
+                                <td className="characteristic-table-score">O-NET Score</td>
+                            </tr>
+                            <tr className="characteristic-row">
+                                <td className="characteristic-table-name">Characteristic</td>
+                                <td className="characteristic-table-score">O-NET Score</td>
+                            </tr>
+                            <tr className="characteristic-row">
+                                <td className="characteristic-table-name">Characteristic</td>
+                                <td className="characteristic-table-score">O-NET Score</td>
+                            </tr>
+                        </table>
                     </div>
-                    {/* race */}
-                    <div className="occupation-graph">
-                        <img alt="exposure to climate of food system worker" src="/test/exposure_to_climate.png"></img>
-                    </div>
-                    {/* citizenship */}
-                    <div className="occupation-graph">
-                        <img alt="exposure to climate of food system worker" src="/test/exposure_to_climate.png"></img>
+                    <div className="occ-sub-container"> 
+                        <h2 className="blue-text">Worker Characteristic Graphs</h2>
+                        {/* exposure to climate for food occupation */}
+                        <div className="occupation-graph">
+                            <img alt="exposure to climate of food system worker" src="/test/exposure_to_climate.png"></img>
+                        </div>
+                        {/* gender */}
+                        <div className="occupation-graph">
+                            <img alt="exposure to climate of food system worker" src="/test/exposure_to_climate.png"></img>
+                        </div>
+                        {/* race */}
+                        <div className="occupation-graph">
+                            <img alt="exposure to climate of food system worker" src="/test/exposure_to_climate.png"></img>
+                        </div>
+                        {/* citizenship */}
+                        <div className="occupation-graph">
+                            <img alt="exposure to climate of food system worker" src="/test/exposure_to_climate.png"></img>
+                        </div>
                     </div>
                 </div>
             </div>
