c1bed8f4d7577071b3678853a7f49b26f2a616b6
angzhang <angzhang@adobe.com>
2022-03-10 10:31:06 -0800

    fix

--- src/pages/Characteristics.js
+++ src/pages/Characteristics.js
@@ -11,9 +11,8 @@ function Characteristics () {
                 <h1>Food System Workers Characteristics</h1>
                 <p>desc</p>
                 <div className="characteristics-table-container">
-                <iframe src="https://public.tableau.com/views/FoodWorkerRace_March/RaceandEthnicityofOccupations?:language=en-US&:display_count=n&:origin=viz_share_link?:showVizHome=no&:embed=true"
-                width="1000" height="955"></iframe> 
-                    {/* <iframe title="Food system worker characteristics tablaeu site" src="https://public.tableau.com/views/FoodWorkersGraphs_16192033166250/OccupationsandRace?:showVizHome=no&:embed=true" width="1000px" height="750px"></iframe> */}
+                    <iframe title="characteristics" src="https://public.tableau.com/views/FoodWorkerRace_March/RaceandEthnicityofOccupations?:language=en-US&:display_count=n&:origin=viz_share_link?:showVizHome=no&:embed=true"
+                    width="1000" height="955"></iframe> 
                 </div>
             </div>
         </div>
